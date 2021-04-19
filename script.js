dbb120c2447242c14271b8997b885c37aae665df
christiaanNagel <61054009+christiaanNagel@users.noreply.github.com>
2021-04-19 22:40:09 +0200

    Final mixed message project

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ let randomNumber = (val) =>
 {
     return Math.floor(Math.random()*val);
 }
+
+
+
+
+let wisdom = 
+{
+    name: ["Pieter","Sarie"],
+    surname: ["Pretorius", "Roux"],
+    kleur: ["blank", "bruin", "swart"]
+}
+
+let randomPerson = (arrayObj) =>
+{
+    let saying = [];
+    for(element in arrayObj)
+    {
+        let option = randomNumber(arrayObj[element].length);
+        saying.push(arrayObj[element][option]);
+    }
+    console.log(`Your name is ${saying[0]} and your surname is ${saying[1]} and your ethnicity is ${saying[2]}`)
+}
+
+randomPerson(wisdom);
